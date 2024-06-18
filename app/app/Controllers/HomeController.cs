ae0222c2727fde6298fc5a514f1f76c672aae3f8
Bakri <abubakerhfb@gmail.com>
2024-06-18 23:57:13 +0300

    Add LogInformation in _logger Variable

---
+++ app/app/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using app.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace app.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            _logger.LogInformation("Index action started");
+            // Your controller logic here
+            _logger.LogInformation("Index action completed");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
